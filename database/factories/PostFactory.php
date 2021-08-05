aafad362f920145c15fa00d8fc0e2d6484940d7c
R Bharadwaj <bwaj95@gmail.com>
2021-08-05 19:54:31 +0530

    Factory Model

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \App\Models\Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "title" => $this->faker->title(),
+            "body" => $this->faker->paragraph(2),
+            "created_at" => now(),
+        ];
+    }
+}
